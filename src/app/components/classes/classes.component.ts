0dabc2372ceb9106cebaa732b211a90e7df3ad69
Michael Zorin <michael.zorin.rd@gmail.com>
2024-08-25 01:47:21 +0200

    classes component, jump-scrolling completed, work on header component

--- src/app/components/classes/classes.component.ts
+++ src/app/components/classes/classes.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-classes',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    CommonModule,
+  ],
   templateUrl: './classes.component.html',
   styleUrl: './classes.component.scss'
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -15,25 +15,25 @@ import { MatButtonModule } from '@angular/material/button';
 export class HeaderComponent implements OnInit {
   private sections: string[] = ['home-section', 'about-section', 'classes-section', 'contacts-section'];
   private currentSectionIndex: number = 0;
+  private isScrolling: boolean = false;
 
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
-    // Check section on initial load
-    this.checkCurrentSection();
-    // Add the wheel event listener as non-passive
+    this.checkCurrentSection(); // Check section on initial load
     window.addEventListener('wheel', this.onWheelScroll.bind(this), { passive: false });
+    window.addEventListener('keydown', this.onKeyDown.bind(this));
   }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    // Check section on scroll
-    this.checkCurrentSection();
+    setTimeout(() => {
+      this.checkCurrentSection(); // Check section on scroll
+    }, 500);
   }
 
   checkCurrentSection() {
-    // Offset for the header height
-    const scrollPosition = window.pageYOffset + 110;
+    const scrollPosition = window.pageYOffset + 110; // Offset for the header height
 
     this.sections.forEach((sectionId, index) => {
       const sectionElement = document.getElementById(sectionId);
@@ -43,33 +43,39 @@ export class HeaderComponent implements OnInit {
 
         if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
           this.currentSectionIndex = index;
-          // console.log('currentSectionIndex is', this.currentSectionIndex);
           this.highlightMenuButton(sectionId);
         }
       }
     });
 
-    // Handle the header transparency for the home section
     const homeSection = document.getElementById('home-section');
     if (homeSection) {
       const sectionTop = homeSection.offsetTop;
       const sectionHeight = homeSection.offsetHeight;
 
       if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-        // this.renderer.addClass(document.body.querySelector('mat-toolbar'), 'semi-transparent');
         this.renderer.addClass(document.body.querySelector('.menu'), 'semi-transparent');
       } else {
-        // this.renderer.removeClass(document.body.querySelector('mat-toolbar'), 'semi-transparent');
         this.renderer.removeClass(document.body.querySelector('.menu'), 'semi-transparent');
       }
     }
   }
 
-  onWheelScroll(event: WheelEvent) {
-    // console.log('event.deltaY', event.deltaY);
-
-    // Prevent the default scrolling behavior
+  manageScrolling(event: WheelEvent | KeyboardEvent) {
+    // Prevent default scrolling
     event.preventDefault();
+    // Ignore if already scrolling
+    if (this.isScrolling) return;
+    // Set the flag to indicate scrolling is in progress
+    this.isScrolling = true;
+  }
+
+  onWheelScroll(event: WheelEvent) {
+
+    event.preventDefault();
+    if (this.isScrolling) return;
+    this.isScrolling = true;
+    // this.manageScrolling(event);
 
     if (event.deltaY > 0) {
       this.scrollToNextSection();
@@ -78,40 +84,59 @@ export class HeaderComponent implements OnInit {
     }
   }
 
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'ArrowDown') {
+
+      event.preventDefault();
+      if (this.isScrolling) return;
+      this.isScrolling = true;
+      // this.manageScrolling(event);
+
+      this.scrollToNextSection();
+    } else if (event.key === 'ArrowUp') {
+
+      event.preventDefault();
+      if (this.isScrolling) return;
+      this.isScrolling = true;
+      // this.manageScrolling(event);
+
+      this.scrollToPreviousSection();
+    }
+  }
+
   scrollToNextSection() {
     if (this.currentSectionIndex < this.sections.length - 1) {
-      // console.log('scrollToNextSection');
-      // console.log('currentSectionIndex', this.currentSectionIndex);
       this.currentSectionIndex++;
-      // console.log('new currentSectionIndex', this.currentSectionIndex);
       this.scrollToSection(this.sections[this.currentSectionIndex]);
+    } else {
+      this.isScrolling = false;  // Reset the flag if no more sections to scroll
     }
   }
 
   scrollToPreviousSection() {
     if (this.currentSectionIndex > 0) {
-      // console.log('scrollToPreviousSection');
-      // console.log('currentSectionIndex', this.currentSectionIndex);
       this.currentSectionIndex--;
       this.scrollToSection(this.sections[this.currentSectionIndex]);
+    } else {
+      this.isScrolling = false;  // Reset the flag if no more sections to scroll
     }
   }
 
   scrollToSection(sectionId: string) {
-    // console.log('scrollToSection to sectionId', sectionId);
     const section = document.getElementById(sectionId);
     if (section) {
-      // console.log('section', section);
-
-      // Enable smooth scrolling between the sections
       section.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+
+      // Estimate when the scrolling is likely to be done
+      setTimeout(() => {
+        this.isScrolling = false;  // Reset the flag after the scroll is likely to have completed
+      }, 500); // Adjust this value if necessary
 
       this.highlightMenuButton(sectionId);
     }
   }
 
   highlightMenuButton(sectionId: string) {
-    // console.log('highlightMenuButton', sectionId);
     const buttons = document.querySelectorAll('.menu button');
     buttons.forEach(button => {
       this.renderer.removeClass(button, 'highlighted');
